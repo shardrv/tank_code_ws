e76a32fb5aa56a3dce6edf1e11319fd1814e074e
Dhruv Sharma <42878758+shardrv@users.noreply.github.com>
2024-12-23 08:58:02 -0600

    Update cooling_tank.h

--- include/cooling_tank.h
+++ include/cooling_tank.h
@@ -1,18 +1,39 @@
+/*
+Cooling Tank Main Header file with function definitions
+class Cooling Tank has Fan and Pump Control individual options 
+to control true/false states of the devices
+Update System updates these states by receiving 0/1/2 integer values
+0 - No action and Pump/Fan OFF
+1 - Operating Range Pump/Fan ON
+2 - High temp range Pump/Fan ON
+
+This also includes separate Safety Check for Over Temp
+
+Added cassert test cases for individual test logging.
+
+- Dhruv Sharma
+*/
+
+#ifndef _cooling_tank_h 
+#define _cooling_tank_h 
+
 #include <iostream>
 #include <random>
 #include <cassert>
 
-const float HI_PUMP_SETPOINT = 20;
-const float HI_HI_PUMP_SETPOINT = 60;
-const float HI_FAN_SETPOINT = 20;
-const float HI_HI_FAN_SETPOINT = 60;
+// Added setpoint values
+// User can make changes here according to requirements
+const float PUMP_SETPOINT = 20;
+const float HI_PUMP_SETPOINT = 60;
+const float FAN_SETPOINT = 20;
+const float HI_FAN_SETPOINT = 60;
 const float TEMP_ALARM = 80;
 
+// Cooling Tank Class
 class Cooling_Tank{
     float temp_sensor_input_IN = 90.0;
-    float level_switch_value = 0.0;
     bool fan_start = false;
-    bool pump_start = true;
+    bool pump_start = false;
     float test_coolant_temp = 0.0;
 
     public:
@@ -21,34 +42,51 @@ class Cooling_Tank{
         int Safety_Control(float temp);
         float temp_simulate(float a, float b);
         void updateSystem(float curr_temp, bool ignition);
+        bool getPumpState();
+        bool getFanState();
         void testControlPump();
         void testControlFan();
         void testSafetyCheck();
+        void setIgnitionSwitch(bool state);
 };
 
+// Get Current Pump State
+bool Cooling_Tank::getPumpState(){
+    return pump_start;
+}
+
+// Get Current Fan State
+bool Cooling_Tank::getFanState(){
+    return fan_start;
+}
+
+// Control Fan using bits 0/1/2
 int Cooling_Tank::Fan_Control(float temp){
 
-    if(temp >= HI_HI_FAN_SETPOINT){
+    if(temp >= HI_FAN_SETPOINT){
         return 2;
     }
-    else if(temp < HI_HI_FAN_SETPOINT && temp >= HI_FAN_SETPOINT){
+    else if(temp < HI_FAN_SETPOINT && temp >= FAN_SETPOINT){
         return 1;
     }
     return 0;
 }
 
+// Control Pump using bits 0/1/2
 int Cooling_Tank::Pump_Control(float temp){
-    if(temp >= HI_HI_PUMP_SETPOINT){
+    if(temp >= HI_PUMP_SETPOINT){
         return 2;
     }
-    else if(temp < HI_HI_PUMP_SETPOINT && temp >= HI_PUMP_SETPOINT){
+    else if(temp < HI_PUMP_SETPOINT && temp >= PUMP_SETPOINT){
         return 1;
     }
     return 0;
 }
 
+// Safety check Over temp
 int Cooling_Tank::Safety_Control(float temp){
-    if(Fan_Control(temp) * Pump_Control(temp) >= 4){
+    // Safety Control not to damage system
+    if(Fan_Control(temp) * Pump_Control(temp) == 4){
             std::cout<<"\nSystem Critical - Pump and Fan turned ON\n";
             fan_start = true;
             pump_start = true;
@@ -57,6 +95,7 @@ int Cooling_Tank::Safety_Control(float temp){
     return 0;
 }
 
+// Simulate temp values in case no user inputs provided
 float Cooling_Tank::temp_simulate(float a, float b) {
     // Random number generator
     std::random_device rd;                     // Obtain a random seed from the system
@@ -67,6 +106,7 @@ float Cooling_Tank::temp_simulate(float a, float b) {
     return dist(gen);
 }
 
+// Execution of system with current values of temp and enable/disable fan/pump
 void Cooling_Tank::updateSystem(float current_temp, bool ignition_switch){
     if(ignition_switch){
         switch(Pump_Control(current_temp)){
@@ -76,7 +116,7 @@ void Cooling_Tank::updateSystem(float current_temp, bool ignition_switch){
                 break;
 
             case 1:
-                std::cout<<"\nTemp has reached a warning value high; pump ON\n";
+                std::cout<<"\nTemp has reached an operating value; pump ON\n";
                 pump_start = true;
                 break;
 
@@ -93,7 +133,7 @@ void Cooling_Tank::updateSystem(float current_temp, bool ignition_switch){
                 break;
 
             case 1:
-                std::cout<<"\nTemp has reached a warning value high; pump ON\n";
+                std::cout<<"\nTemp has reached an operating value; pump ON\n";
                 fan_start = true;
                 break;
 
@@ -120,7 +160,7 @@ void Cooling_Tank::testControlPump() {
     TestCase testCases[] = {
         {15.0, 0},  // Temperature below MIN_COOLANT_TEMP
         {40.0, 1},   // Temperature above MIN_COOLANT_TEMP
-        {90.0, 2}    // Temperature exactly MIN_COOLANT_TEMP
+        {65.0, 2}    // Temperature sending alarm
     };
 
     // Run the tests
@@ -144,7 +184,7 @@ void Cooling_Tank::testControlFan() {
     TestCase testCases[] = {
         {15.0, 0},  // Temperature below FAN_START_TEMP
         {65.0, 2},   // Temperature above FAN_START_TEMP
-        {55.0, 1},  // Temperature below FAN_STOP_TEMP
+        {55.0, 1},  // Temperature just below operating range
         {85.0, 2}    // Temperature above FAN_START_TEMP
     };
 
@@ -181,3 +221,4 @@ void Cooling_Tank::testSafetyCheck() {
 
     std::cout << "safetyCheck tests passed.\n";
 }
+#endif
